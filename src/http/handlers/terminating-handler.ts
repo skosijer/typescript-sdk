c1e03bfa23dbeeae6fd16f4ffdc17397b738a551
Stevan Kosijer <stkosijer@gmail.com>
2024-03-20 20:19:26 +0100

    SDK update generated by liblab

---
+++ src/http/handlers/terminating-handler.ts
@@ -0,0 +1,30 @@
+import { HttpRequest, HttpResponse, RequestHandler } from '../types';
+
+export class TerminatingHandler implements RequestHandler {
+  async handle<T>({ url, method, body, abortSignal, headers }: HttpRequest<T>): Promise<HttpResponse<T>> {
+    const response = await fetch(url, {
+      method,
+      body,
+      signal: abortSignal,
+      headers,
+    });
+
+    return {
+      metadata: {
+        status: response.status,
+        statusText: response.statusText,
+        headers: this.getHeaders(response),
+      },
+      body: await response.clone().arrayBuffer(),
+    };
+  }
+
+  private getHeaders(response: Response): Record<string, string> {
+    const headers: Record<string, string> = {};
+    response.headers.forEach((value, key) => {
+      headers[key] = value;
+    });
+
+    return headers;
+  }
+}
