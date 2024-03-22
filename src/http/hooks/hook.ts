33cf76b1a5e698873f1a90e978cbaf52d7307b68
Stevan Kosijer <stkosijer@gmail.com>
2024-03-22 15:55:23 +0100

    liblab SDK update for version v4.0.0

---
+++ src/http/hooks/hook.ts
@@ -0,0 +1,35 @@
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+export interface HttpRequest {
+  method: HttpMethod;
+  url: string;
+  headers?: Record<string, string>;
+  body?: BodyInit;
+  abortSignal?: AbortSignal;
+  queryParams?: Record<string, unknown>;
+}
+
+interface HttpMetadata {
+  status: number;
+  statusText: string;
+  headers: Record<string, string>;
+}
+
+export interface HttpResponse<T> {
+  data?: T;
+  metadata: HttpMetadata;
+  body: ArrayBuffer;
+}
+
+export interface HttpError {
+  error: string;
+  metadata: HttpMetadata;
+}
+
+export interface Hook {
+  beforeRequest(request: HttpRequest): HttpRequest;
+
+  afterResponse(request: HttpRequest, response: HttpResponse<any>): HttpResponse<any>;
+
+  onError(request: HttpRequest, response: HttpResponse<any>): HttpError;
+}
